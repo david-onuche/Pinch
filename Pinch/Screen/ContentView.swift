73d8ada7531391186c39e85b8ce2f8c8b473ba23
davidonuche <iamdavidonuche@gmail.com>
2026-09-08 20:08:07 +0100

    Add asset catalog and initial ContentView implementation

---
+++ Pinch/Screen/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by David Onuche on 08/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    // MARK: - FUNCTION
+    
+    // MARK: - CONTENT
+    
+    var body: some View {
+       Text("David Onuche")
+            .padding()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 17 Pro")
+    }
+}
