3f49d47dcc57a096ce3646944f73e635d3553a5f
davidonuche <iamdavidonuche@gmail.com>
2026-09-09 15:17:59 +0100

    Page Thumbnails

---
+++ Pinch/Data/PageData.swift
@@ -0,0 +1,18 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by David Onuche on 09/09/2026.
+//
+
+import SwiftUI
+
+struct PageData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageData()
+}
---
+++ Pinch/Model/PageModel.swift
@@ -0,0 +1,18 @@
+//
+//  PageModel.swift
+//  Pinch
+//
+//  Created by David Onuche on 09/09/2026.
+//
+
+import SwiftUI
+
+struct PageModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageModel()
+}
